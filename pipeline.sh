024ee4c133e5224f283b69e7390eed235736866e
fengzijian <393390162@qq.com>
2022-11-09 01:58:14 +0800

    feat: 增加流水线

---
+++ pipeline.sh
@@ -0,0 +1,8 @@
+rm -rf dashboard
+git clone  -b develop http://root:Admin123%21%40%23@192.168.3.100:88/midas/dashboard.git
+cd dashboard
+mvn clean package -Dmaven.test.skip=true
+docker rm -f $(docker ps --filter "name=dashboard" -q)
+docker images | grep "dashboard" | awk '{print $1":"$2}' | xargs docker rmi
+docker build -t dashboard .
+docker run --name my_dashboard  -p 8877:8080 -d dashboard
