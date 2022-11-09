6e01ea3b9d7f2acce55eb8e48c900fb11297c152
fengzijian <393390162@qq.com>
2022-11-09 21:57:30 +0800

    feat: 截图用的Driver复用

--- src/main/java/com/zero/dashboard/component/TradeScreenshot.java
+++ src/main/java/com/zero/dashboard/component/TradeScreenshot.java
@@ -21,7 +21,9 @@ import java.time.Duration;
 
 public class TradeScreenshot {
 
-    public void exec(String url, String filePath){
+    private static final WebDriver DRIVER;
+
+    static {
         ChromeOptions options = new ChromeOptions();
         if(new OsInfo().isLinux()){
             options.setBinary("/opt/google/chrome/chrome");
@@ -30,22 +32,36 @@ public class TradeScreenshot {
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
-        driver.get(url);
-        Actions actions = new Actions(driver);
+
+        DRIVER = driver;
+    }
+
+
+    public void exec(String url, String filePath){
+//        ChromeOptions options = new ChromeOptions();
+//        if(new OsInfo().isLinux()){
+//            options.setBinary("/opt/google/chrome/chrome");
+//            options.addArguments("--headless", "--no-sandbox", "--window-size=1920,1080");
+//        }
+//        WebDriver driver = new ChromeDriver(options);
+//        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//        driver.manage().window().maximize();
+        DRIVER.get(url);
+        Actions actions = new Actions(DRIVER);
         // 鼠标移动到一侧
         actions.moveByOffset(920, 0).perform();
-        WebDriverWait wait = new WebDriverWait(driver,5);
+        WebDriverWait wait = new WebDriverWait(DRIVER,5);
         wait.until(ExpectedConditions.elementToBeClickable(By.id("kline"))).click();
         wait.until(ExpectedConditions.elementToBeClickable(By.id("volume"))).click();
         wait.until(ExpectedConditions.elementToBeClickable(By.id("macd"))).click();
         wait.until(ExpectedConditions.elementToBeClickable(By.id("kdj"))).click();
-        Screenshot screenshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(100)).takeScreenshot(driver);
+        Screenshot screenshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(100)).takeScreenshot(DRIVER);
         BufferedImage image = screenshot.getImage();
         try {
             ImageIO.write(image, "PNG", new File(filePath));
         } catch (IOException e) {
             e.printStackTrace();
         }
-        driver.close();
+//        driver.close();
     }
 }
