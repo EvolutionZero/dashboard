bfde820324ce81b44cb33b5c017dec3f55e88b3a
fengzijian <393390162@qq.com>
2022-11-10 00:38:31 +0800

    feat: 慢操作排查

--- src/main/java/com/zero/dashboard/component/TradeScreenshot.java
+++ src/main/java/com/zero/dashboard/component/TradeScreenshot.java
@@ -1,7 +1,6 @@
 package com.zero.dashboard.component;
 
 import cn.hutool.core.date.StopWatch;
-import cn.hutool.json.JSONUtil;
 import cn.hutool.system.OsInfo;
 import lombok.extern.slf4j.Slf4j;
 import org.openqa.selenium.By;
@@ -19,11 +18,9 @@ import ru.yandex.qatools.ashot.shooting.ShootingStrategies;
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.time.Duration;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 @Slf4j
 public class TradeScreenshot {
@@ -36,7 +33,7 @@ public class TradeScreenshot {
             options.setBinary("/opt/google/chrome/chrome");
             options.addArguments("--headless", "--no-sandbox", "--window-size=1920,1080");
         }
-        ChromeDriver driver = new ChromeDriver(options);
+        WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
         stopWatch.stop();
@@ -56,52 +53,23 @@ public class TradeScreenshot {
         wait.until(ExpectedConditions.elementToBeClickable(By.id("kdj"))).click();
         stopWatch.stop();
 
-//        stopWatch.start("截图");
-//        Screenshot screenshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(100)).takeScreenshot(driver);
-//        stopWatch.stop();
-//
-//        stopWatch.start("保存图片");
-//        BufferedImage image = screenshot.getImage();
-//        try {
-//            ImageIO.write(image, "PNG", new File(filePath));
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//        stopWatch.stop();
+        stopWatch.start("截图");
+        Screenshot screenshot = new AShot().shootingStrategy(ShootingStrategies.viewportPasting(100)).takeScreenshot(driver);
+        stopWatch.stop();
 
-//        stopWatch.start("关闭浏览器");
-//        stopWatch.stop();
-
-        stopWatch.start("命令截图");
-        String command = "Page.captureEntirePageScreenshot";
-        Object width = driver.executeScript("return document.body.scrollWidth");
-        Object height = driver.executeScript("return document.body.scrollHeight");
-
-        Map<String, Object> params = new HashMap();
-        params.put("width", width);
-        params.put("height", height);
-        params.put("fromSurface", true);
-        params.put("mobile", false);
-        params.put("deviceScaleFactor", 1);
-
-        driver.executeCdpCommand("Emulation.setDeviceMetricsOverride", params);
-
-        Map<String, Object> output = driver.executeCdpCommand(command, params);
-//        log.info("命令截图: " + JSONUtil.toJsonStr(output));
-//        log.info("命令截图: " + JSONUtil.toJsonStr(output.keySet()));
-        log.info("截图参数: " + JSONUtil.toJsonStr(params));
+        stopWatch.start("保存图片");
+        BufferedImage image = screenshot.getImage();
         try {
-            FileOutputStream fileOutputStream = new FileOutputStream(filePath);
-            byte[] byteArray = java.util.Base64.getDecoder().decode((String) output.get("data"));
-            fileOutputStream.write(byteArray);
-            fileOutputStream.close();
+            ImageIO.write(image, "PNG", new File(filePath));
         } catch (IOException e) {
-            log.error("", e);
+            e.printStackTrace();
         }
         stopWatch.stop();
 
+        stopWatch.start("关闭浏览器");
         driver.close();
+        stopWatch.stop();
 
-        log.info(stopWatch.prettyPrint());
+        log.info(stopWatch.prettyPrint(TimeUnit.MILLISECONDS));
     }
 }
