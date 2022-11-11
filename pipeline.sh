e22da35a3d4b0e0c9f89448a030cc4af0e9176da
fengzijian <393390162@qq.com>
2022-11-11 23:35:23 +0800

    feat: 多镜像部署

--- pipeline.sh
+++ pipeline.sh
@@ -1,8 +1,9 @@
-rm -rf dashboard
-git clone  -b develop http://root:Admin123%21%40%23@192.168.3.100:88/midas/dashboard.git
 cd dashboard
 mvn clean package -Dmaven.test.skip=true
 docker rm -f $(docker ps --filter "name=dashboard" -q -a)
 docker images | grep "dashboard" | awk '{print $1":"$2}' | xargs docker rmi
 docker build -t dashboard .
-docker run --name my_dashboard --cpus="0.5" -m 512m --restart=always  -p 8877:8081 -d dashboard
+docker run --name my_dashboard-1 --restart=always  -p 8877:8081 -d dashboard
+docker run --name my_dashboard-2 --restart=always  -p 8878:8081 -d dashboard
+docker run --name my_dashboard-3 --restart=always  -p 8879:8081 -d dashboard
+docker run --name my_dashboard-4 --restart=always  -p 8880:8081 -d dashboard
