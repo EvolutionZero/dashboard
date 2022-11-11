a18db0bfd453fd54d5cd2b3ae92d1f6e1b16cbb9
fengzijian <393390162@qq.com>
2022-11-11 23:42:18 +0800

    feat: 多镜像部署

--- pipeline.sh
+++ pipeline.sh
@@ -4,4 +4,3 @@ docker rm -f $(docker ps --filter "name=dashboard" -q -a)
 docker images | grep "dashboard" | awk '{print $1":"$2}' | xargs docker rmi
 docker build -t dashboard .
 docker run --name my_dashboard-1 --restart=always  -p 8877:8081 -d dashboard
-docker run --name my_dashboard-2 --restart=always  -p 8878:8081 -d dashboard
