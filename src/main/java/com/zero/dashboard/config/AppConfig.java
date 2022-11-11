de7083f37ac904f684d3c7e05e7a81a45634e9b7
fengzijian <393390162@qq.com>
2022-11-11 23:49:24 +0800

    feat: 多镜像部署

--- src/main/java/com/zero/dashboard/config/AppConfig.java
+++ src/main/java/com/zero/dashboard/config/AppConfig.java
@@ -17,44 +17,44 @@ import java.util.concurrent.*;
 public class AppConfig {
 
 
-    @Bean(name = "drivers")
-    public ThreadPoolExecutor pool(){
-        int factor = 2;
-        int availableProcessors = Runtime.getRuntime().availableProcessors() * factor;
-        log.info("浏览器驱动线程数[{}]", availableProcessors);
-        return new ThreadPoolExecutor(
-                availableProcessors, availableProcessors,
-                10L, TimeUnit.SECONDS,
-                new LinkedBlockingQueue(),Executors.defaultThreadFactory()
-        );
-    }
+//    @Bean(name = "drivers")
+//    public ThreadPoolExecutor pool(){
+//        int factor = 2;
+//        int availableProcessors = Runtime.getRuntime().availableProcessors() * factor;
+//        log.info("浏览器驱动线程数[{}]", availableProcessors);
+//        return new ThreadPoolExecutor(
+//                availableProcessors, availableProcessors,
+//                10L, TimeUnit.SECONDS,
+//                new LinkedBlockingQueue(),Executors.defaultThreadFactory()
+//        );
+//    }
 
 
 
-    @Bean(name = "chromes")
-    public LinkedBlockingQueue<WebDriver> chromes(){
-        LinkedBlockingQueue chromes = new LinkedBlockingQueue();
-        for (int i = 0; i < 1; i++) {
-            ChromeOptions options = new ChromeOptions();
-            if(new OsInfo().isLinux()){
-                options.setBinary("/opt/google/chrome/chrome");
-                options.addArguments(
-                        "--headless",
-                        "--no-sandbox",
-                        "--window-size=1920,1080" ,
-                        "--disable-dev-shm-usage",
-                        "--disk-cache-dir=/opt/selenium/opt/dashboard/cache");
-                options.setCapability("pageLoadStrategy", "none");
-            }
-            WebDriver driver = new ChromeDriver(options);
-            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-            driver.manage().window().maximize();
-            Actions actions = new Actions(driver);
-            actions.moveByOffset(920, 0).perform();
-
-            chromes.add(driver);
-        }
-        return chromes;
-    }
+//    @Bean(name = "chromes")
+//    public LinkedBlockingQueue<WebDriver> chromes(){
+//        LinkedBlockingQueue chromes = new LinkedBlockingQueue();
+//        for (int i = 0; i < 1; i++) {
+//            ChromeOptions options = new ChromeOptions();
+//            if(new OsInfo().isLinux()){
+//                options.setBinary("/opt/google/chrome/chrome");
+//                options.addArguments(
+//                        "--headless",
+//                        "--no-sandbox",
+//                        "--window-size=1920,1080" ,
+//                        "--disable-dev-shm-usage",
+//                        "--disk-cache-dir=/opt/selenium/opt/dashboard/cache");
+//                options.setCapability("pageLoadStrategy", "none");
+//            }
+//            WebDriver driver = new ChromeDriver(options);
+//            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//            driver.manage().window().maximize();
+//            Actions actions = new Actions(driver);
+//            actions.moveByOffset(920, 0).perform();
+//
+//            chromes.add(driver);
+//        }
+//        return chromes;
+//    }
 
 }
